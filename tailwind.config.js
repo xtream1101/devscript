61d0c2f1b54646205176f75d1e29bd2c486f7add
Thalida Noel <hello@thalida.com>
2025-01-27 23:18:01 -0500

    Shared styles for highlight contianer; Remove tailwind highlighting

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
         'sans': ['Outfit', '"Inter var"', 'Inter', 'sans-serif'],
         'mono': ['"Fira Mono"', '"JetBrains Mono"', 'monospace'],
       },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            pre: null,
+            code: null,
+            'code::before': null,
+            'code::after': null,
+            'pre code': null,
+            'pre code::before': null,
+            'pre code::after': null
+          },
+        },
+      }),
     },
   },
   plugins: [
